e4f22bea6e40cb2bebf11318076b0f82c77a2967
RosielArcamo <arcamo.rosiel@cec.edu.ph>
2025-05-03 19:15:20 +0800

    final commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Flask on Railway!"
+    return "Hello from flask mfkdmfkmdsfds!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
